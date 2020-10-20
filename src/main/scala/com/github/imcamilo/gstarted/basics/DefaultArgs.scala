73d5c48c1f23bf7cc1b65f3977f4c33f52be287f
Camilo Jorquera <imcamilo@Camilos-MacBook-Pro.local>
2020-10-20 19:31:50 -0300

    feat: call by name and calle by value, deafult args and smart strings

---
+++ src/main/scala/com/github/imcamilo/gstarted/basics/DefaultArgs.scala
@@ -0,0 +1,17 @@
+package com.github.imcamilo.gstarted.basics
+
+object DefaultArgs extends App {
+
+  def trFact(n: Int, acc: Int = 1): Int =
+    if (n <= 1) acc
+    else trFact(n - 1, n * acc)
+
+  val factorial10 = trFact(10)
+
+  def savePicture(format: String = "jpg", width: Int = 1920, height: Int = 1080) = println("saved")
+
+  savePicture()
+  savePicture(width = 200)
+  savePicture(height = 300, width = 654, format = "pdf")
+
+}

6e5ec93653222152778a38f067d85db4d7ea3a22
Camilo Jorquera <camilo.jorquera.a@gmail.com>
2021-02-09 23:49:09 -0300

    feat(case-class): case class feature added

---
+++ src/main/scala/com/github/imcamilo/gstarted/oop/CaseClasses.scala
@@ -0,0 +1,38 @@
+package com.github.imcamilo.gstarted.oop
+
+object CaseClasses extends App {
+
+  //equals, hashCode, toString
+
+  case class Person(name: String, age: Int)
+
+  //1. Class parameters are promoted to fields
+  val cml = new Person("Camilo", 26)
+  println(cml.name)
+
+  //2. Sensible toString
+  //println(instance) == println(instance.toString) //syntactic sugar
+  println(cml)
+
+  //3. Equals and HashCode implemented OOB
+  val cml2 = new Person("Camilo", 26)
+  println(cml == cml2) //only with case classes with equals method implemented
+
+  //4. Case Classes have handy copy method
+  val cml3 = cml.copy(age = 20)
+  println(cml3)
+
+  //5. Case Classes have companion objects
+  val thePerson = Person
+  val mary = Person("Mary", 23)
+
+  //6. Case Classes are serializable
+  //Akka
+
+  //7. Case Classes have extractors patterns = CCs can be used in Pattern Matching
+
+  case object RepOfChile {
+    def name: String = "Republic Of Chile"
+  }
+
+}

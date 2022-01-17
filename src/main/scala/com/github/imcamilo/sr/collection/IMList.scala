e450028264651bc55d78af4e25c46cfb51f24f9c
Camilo Jorquera <camilo.jorquera.a@gmail.com>
2022-01-17 01:45:58 -0300

    feat(IMList): updating typo

--- src/main/scala/com/github/imcamilo/sr/collection/IMList.scala
+++ src/main/scala/com/github/imcamilo/sr/collection/IMList.scala
@@ -3,49 +3,30 @@ package com.github.imcamilo.sr.collection
 object IMListIntTest extends App {
 
   abstract class IMList {
-
     def head: Int
-
     def tail: IMList
-
     def isEmpty: Boolean
-
     def add(element: Int): IMList
-
     def printElements: String
-
     override def toString: String = s"[$printElements]"
-
   }
 
   object Empty extends IMList {
-
-    override def head: Int = throw new NoSuchElementException
-
-    override def tail: IMList = throw new NoSuchElementException
-
-    override def isEmpty: Boolean = true
-
-    override def add(element: Int): IMList = new Cons(element, Empty)
-
-    override def printElements: String = ""
-
+    def head: Int = throw new NoSuchElementException
+    def tail: IMList = throw new NoSuchElementException
+    def isEmpty: Boolean = true
+    def add(element: Int): IMList = new Cons(element, Empty)
+    def printElements: String = ""
   }
 
   class Cons(hd: Int, tl: IMList) extends IMList {
-
-    override def head: Int = hd
-
-    override def tail: IMList = tl
-
-    override def isEmpty: Boolean = false
-
-    override def add(element: Int): IMList = new Cons(element, tl)
-
-    override def printElements: String =
+     def head: Int = hd
+     def tail: IMList = tl
+     def isEmpty: Boolean = false
+     def add(element: Int): IMList = new Cons(element, tl)
+     def printElements: String =
       if (tl.isEmpty) "" + hd
       else hd + " " + tl.printElements
-
   }
 
   val imList = new Cons(1, Empty)
@@ -62,56 +43,36 @@ object IMListIntTest extends App {
 object IMListGenericsTest extends App {
 
   abstract class IMList[+A] {
-
     def head: A
-
     def tail: IMList[A]
-
     def isEmpty: Boolean
-
     def add[B >: A](element: B): IMList[B]
-
     def printElements: String
-
     override def toString: String = s"[$printElements]"
-
   }
 
   object Empty extends IMList[Nothing] {
-
-    override def head: Nothing = throw new NoSuchElementException
-
-    override def tail: IMList[Nothing] = throw new NoSuchElementException
-
-    override def isEmpty: Boolean = true
-
-    override def add[B >: Nothing](element: B): IMList[B] = new Cons(element, Empty)
-
-    override def printElements: String = ""
-
+    def head: Nothing = throw new NoSuchElementException
+    def tail: IMList[Nothing] = throw new NoSuchElementException
+    def isEmpty: Boolean = true
+    def add[B >: Nothing](element: B): IMList[B] = new Cons(element, Empty)
+    def printElements: String = ""
   }
 
   class Cons[+A](hd: A, tl: IMList[A]) extends IMList[A] {
-
-    override def head: A = hd
-
-    override def tail: IMList[A] = tl
-
-    override def isEmpty: Boolean = false
-
-    override def add[B >: A](element: B): IMList[B] = new Cons(element, tl)
-
-    override def printElements: String =
+    def head: A = hd
+    def tail: IMList[A] = tl
+    def isEmpty: Boolean = false
+    def add[B >: A](element: B): IMList[B] = new Cons(element, tl)
+    def printElements: String =
       if (tl.isEmpty) "" + hd
       else hd + " " + tl.printElements
-
   }
 
   val listOfIntegers: IMList[Int] = new Cons(1, new Cons(2, new Cons(3, new Cons(4, Empty))))
   val listOfStrings: IMList[String] = new Cons("Hello", new Cons("Scala", new Cons("World", Empty)))
   println(listOfIntegers.toString)
   println(listOfStrings.toString)
-
   println(listOfIntegers)
 
 }
@@ -119,88 +80,49 @@ object IMListGenericsTest extends App {
 object IMListLatest extends App {
 
   abstract class IMList[+A] {
-
     def head: A
-
     def tail: IMList[A]
-
     def isEmpty: Boolean
-
     def add[B >: A](element: B): IMList[B]
-
     def printElements: String
-
-    override def toString: String = s"[$printElements]"
-
-    //high order functions
-    def map[B](transformer: A => B): IMList[B]
-
+    def map[B](transformer: A => B): IMList[B] ////high order functions
     def filter(test: A => Boolean): IMList[A]
-
     def flatMap[B](transformer: A => IMList[B]): IMList[B]
-
-    //concatenation
-    def ++[B >: A](imList: IMList[B]): IMList[B]
-
-    //hofs
-    def foreach(f: A => Unit): Unit
-
+    def ++[B >: A](imList: IMList[B]): IMList[B] //concatenation
+    def foreach(f: A => Unit): Unit //hofs
     def sort(compare: (A, A) => Int): IMList[A]
-
     def zipWith[B, C](list: IMList[B], zip: (A, B) => C): IMList[C]
-
     def fold[B](start: B)(operator: (B, A) => B): B
-
+    override def toString: String = s"[$printElements]"
   }
 
   case object Empty extends IMList[Nothing] {
-
-    override def head: Nothing = throw new NoSuchElementException
-
-    override def tail: IMList[Nothing] = throw new NoSuchElementException
-
-    override def isEmpty: Boolean = true
-
-    override def add[B >: Nothing](element: B): IMList[B] = new Cons(element, Empty)
-
-    override def printElements: String = ""
-
-    override def map[B](transformer: Nothing => B): IMList[B] = Empty
-
-    override def filter(test: Nothing => Boolean): IMList[Nothing] = Empty
-
-    override def flatMap[B](transformer: Nothing => IMList[B]): IMList[B] = Empty
-
-    override def ++[B >: Nothing](imList: IMList[B]): IMList[B] = imList
-
-    //hofs
-    override def foreach(f: Nothing => Unit): Unit = ()
-
-    override def sort(f: (Nothing, Nothing) => Int) = Empty
-
-    override def zipWith[B, C](list: IMList[B], zip: (Nothing, B) => C): IMList[C] = {
+    def head: Nothing = throw new NoSuchElementException
+    def tail: IMList[Nothing] = throw new NoSuchElementException
+    def isEmpty: Boolean = true
+    def add[B >: Nothing](element: B): IMList[B] = new Cons(element, Empty)
+    def printElements: String = ""
+    def map[B](transformer: Nothing => B): IMList[B] = Empty
+    def filter(test: Nothing => Boolean): IMList[Nothing] = Empty
+    def flatMap[B](transformer: Nothing => IMList[B]): IMList[B] = Empty
+    def ++[B >: Nothing](imList: IMList[B]): IMList[B] = imList
+    def foreach(f: Nothing => Unit): Unit = () //hofs
+    def sort(f: (Nothing, Nothing) => Int) = Empty
+    def zipWith[B, C](list: IMList[B], zip: (Nothing, B) => C): IMList[C] = {
       if (!list.isEmpty) throw new RuntimeException("list dont have the same length")
       else Empty
     }
-
     override def fold[B](start: B)(operator: (B, Nothing) => B): B = start
-
   }
 
   case class Cons[+A](hd: A, tl: IMList[A]) extends IMList[A] {
-
-    override def head: A = hd
-
-    override def tail: IMList[A] = tl
-
-    override def isEmpty: Boolean = false
-
-    override def add[B >: A](element: B): IMList[B] = new Cons(element, tl)
-
-    override def printElements: String =
+    def head: A = hd
+    def tail: IMList[A] = tl
+    def isEmpty: Boolean = false
+    def add[B >: A](element: B): IMList[B] = new Cons(element, tl)
+    def printElements: String =
       if (tl.isEmpty) "" + hd
       else hd + " " + tl.printElements
-
     /*
      * [1,2,3,4].map(n * 2)
      * = new Cons(2, [2,3,4].map(n * 2))
@@ -209,9 +131,8 @@ object IMListLatest extends App {
      * = new Cons(2, new Cons(4, new Cons(6, new Cons(8, Empty.map(n * 2)))))
      * = new Cons(2, new Cons(4, new Cons(6, new Cons(8, Empty))))
      */
-    override def map[B](transformer: A => B): IMList[B] =
+    def map[B](transformer: A => B): IMList[B] =
       new Cons(transformer(hd), tl.map(transformer))
-
     /*
      * [1,2,3,4].filter(n % 2 == 0)
      * [2,3,4].filter(n % 2 == 0)
@@ -221,10 +142,9 @@ object IMListLatest extends App {
      * = new Cons(2, new Cons(4, Empty.filter(n % 2 == 0)))
      * = new Cons(2, new Cons(4, Empty))
      */
-    override def filter(predicate: A => Boolean): IMList[A] =
+    def filter(predicate: A => Boolean): IMList[A] =
       if (predicate(hd)) new Cons(hd, tl.filter(predicate))
       else tl.filter(predicate)
-
     /*
      * [1,2] ++ [3,4,5]
      * new Cons(1, [2] ++ [3,4,5])
@@ -232,8 +152,7 @@ object IMListLatest extends App {
      * new Cons(1, new Cons(2, [3,4,5]))
      * new Cons(1, new Cons(2, new Cons(3, new Cons(4), new Cons(5))))
      */
-    override def ++[B >: A](imList: IMList[B]): IMList[B] = new Cons(hd, tl ++ imList)
-
+    def ++[B >: A](imList: IMList[B]): IMList[B] = new Cons(hd, tl ++ imList)
     /*
      * [1,2].flatMap(n => [n, n+1])
      * [1,2] ++ [2].flatMap(n => [n, n+1])
@@ -241,31 +160,24 @@ object IMListLatest extends App {
      * [1,2] ++ [2,3] ++ Empty
      * [1,2,2,3]
      */
-    override def flatMap[B](transformer: A => IMList[B]): IMList[B] =
+    def flatMap[B](transformer: A => IMList[B]): IMList[B] =
       transformer(hd) ++ tl.flatMap(transformer)
-
-    //hofs
-
-    override def foreach(f: A => Unit): Unit = {
+    def foreach(f: A => Unit): Unit = { //hofs
       f(head)
       tail.foreach(f)
     }
-
-    override def sort(compare: (A, A) => Int): IMList[A] = {
+    def sort(compare: (A, A) => Int): IMList[A] = {
       def insert(x: A, sortedList: IMList[A]): IMList[A] = {
         if (sortedList.isEmpty) new Cons(x, Empty)
         else if (compare(x, sortedList.head) <= 0) new Cons(x, sortedList)
-        else new Cons(sortedList.head, insert(x, sortedList.tail))
+        else Cons(sortedList.head, insert(x, sortedList.tail))
       }
-
       val sortedTail = tail.sort(compare)
       insert(head, sortedTail)
     }
-
-    override def zipWith[B, C](list: IMList[B], zip: (A, B) => C): IMList[C] =
+    def zipWith[B, C](list: IMList[B], zip: (A, B) => C): IMList[C] =
       if (list.isEmpty) throw new RuntimeException("list dont have the same length")
       else new Cons[C](zip(head, list.head), tail.zipWith(list.tail, zip))
-
     /*
       [1,2,3].fold(0)(+) =
       = [2, 3].fold(1)(+) =
@@ -273,7 +185,7 @@ object IMListLatest extends App {
       = [].fold(6)(+)
       = 6
      */
-    override def fold[B](start: B)(operator: (B, A) => B): B = {
+    def fold[B](start: B)(operator: (B, A) => B): B = {
       val newStart = operator(start, head)
       tail.fold(newStart)(operator)
     }

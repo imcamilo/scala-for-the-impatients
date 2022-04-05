da9ba79276e8dafd383280051d108c81a833d1f7
Camilo Jorquera <camilo.jorquera.a@gmail.com>
2022-04-04 23:44:52 -0400

    chore(circe): using automatic derivation

---
+++ src/main/scala/com/github/imcamilo/circe/AutomaticDerivation.scala
@@ -0,0 +1,19 @@
+package com.github.imcamilo.circe
+
+object AutomaticDerivation extends App {
+
+  import io.circe.syntax._
+  import io.circe.{Decoder, Encoder}
+  // this is considered harmfull, prefer semi-automatic derivation
+  // encoders and decoders, works well but maybe with boilerplate
+
+  import io.circe.generic.auto._ // deriving for us encoder and decoders any case class
+
+  implicitly[Encoder[Author]]
+  implicitly[Encoder[Article]]
+  implicitly[Decoder[Author]]
+  implicitly[Decoder[Article]]
+
+  println(Author("JRR", Some("something")).asJson.spaces2)
+
+}
